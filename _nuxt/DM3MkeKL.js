ec6cb33da28d252bd95669099880b10ed4a777c6
ThornWalli <ThornWalli@users.noreply.github.com>
2025-12-29 11:36:44 +0000

    deploy: 9498a7d806816bd88601d2d0f08d762e6a95ebbf

---
+++ _nuxt/DM3MkeKL.js
@@ -0,0 +1,1 @@
+import{m as n}from"./DdtJlJcl.js";const e=new n({id:"blue-faction",name:"Blue Faction",colors:[21930,13421772]}),a=new n({id:"enemy-faction",name:"Enemy Faction",colors:[2117648,10530976]});export{e as b,a as e};
