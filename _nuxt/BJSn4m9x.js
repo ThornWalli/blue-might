a8e47261b9099919960b107bd70193ec6a91225f
ThornWalli <ThornWalli@users.noreply.github.com>
2026-01-04 21:53:44 +0000

    deploy: b3d1a2eebe6cc4a9e254827ae0ae9eb8e133c8bd

---
+++ _nuxt/BJSn4m9x.js
@@ -0,0 +1,1 @@
+import{J as o}from"./h22pyQnP.js";const a=new o({id:"blue-faction",name:"Blue Faction",colors:[21930,13421772],mapColor:21930}),e=new o({id:"enemy-faction",name:"Enemy Faction",colors:[2117648,10530976],mapColor:16711680});export{a as b,e};
