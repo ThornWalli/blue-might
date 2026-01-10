9005d70fdd34658678f5825ba1051f2e50b08d18
ThornWalli <ThornWalli@users.noreply.github.com>
2026-01-10 23:07:10 +0000

    deploy: fa68971ee837b00a75edd1a4104d524fedef32b0

---
+++ _nuxt/TZCRs901.js
@@ -0,0 +1,1 @@
+import{b7 as o}from"./DxsyhA2U.js";const a=new o({id:"blue-faction",name:"Blue Faction",colors:[21930,13421772],mapColor:21930}),e=new o({id:"enemy-faction",name:"Enemy Faction",colors:[2117648,10530976],mapColor:16711680});export{a as b,e};
