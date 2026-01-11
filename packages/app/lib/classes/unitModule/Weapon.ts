64cda6e9b13584943f49a95024c37dab67e7e5cc
Thorn Walli <lammpee@gmail.com>
2026-01-11 18:03:43 +0100

    fix(app): call abortShoot on unit destruction

--- packages/app/lib/classes/unitModule/Weapon.ts
+++ packages/app/lib/classes/unitModule/Weapon.ts
@@ -156,6 +156,7 @@ export default class WeaponUnitModule<
 
     this.subscription.add(
       unit.modules.damage.observables.destroyed$.subscribe(() => {
+        this.abortShoot();
         this.destroy();
       })
     );
